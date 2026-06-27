430ab894e50a644d95f663b9254a3b8347798cba
samcheck-rgb <Party.Island.Gamers@gmail.com>
2026-06-27 23:03:14 +0300

    Add smooth sidebar navigation

---
+++ js/navigation.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.sidebar a').forEach(link => {
+  link.addEventListener('click', e => {
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
